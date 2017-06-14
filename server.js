94c32f83a251d46c6dbe0d57bbc56771d933ea91
martabrzeszczyk <martabrzeszczyk@gmail.com>
2017-06-14 02:12:48 +0200

    initial

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
+  console.log('aa');
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
